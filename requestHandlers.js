94b109d1820b44d1cad55c6b4e145507ad867ef1
xieQin <xieqin1991@gmail.com>
2014-07-10 17:17:08 +0800

    add sleep functon in requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
 	console.log("Request handler 'start' was called.");
+
+	function sleep(milliSeconds) {
+		var starTime = new Date().getTime();
+		while(new Date().getTime() < starTime + milliSeconds);
+	}
+
+	sleep(10000);
 	return "Hello start";
 }
 
