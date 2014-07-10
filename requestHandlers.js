74255beed5891305bc2685d7cae242721de72589
xieQin <xieqin1991@gmail.com>
2014-07-10 17:29:21 +0800

    modify requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,20 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("Request handler 'start' was called.");
+	var content = "empty";
 
-	function sleep(milliSeconds) {
-		var starTime = new Date().getTime();
-		while(new Date().getTime() < starTime + milliSeconds);
-	}
+	exec("find", function(error, stdout, stderr) {
+		content = stdout;
+	})
+	// function sleep(milliSeconds) {
+	// 	var starTime = new Date().getTime();
+	// 	while(new Date().getTime() < starTime + milliSeconds);
+	// }
 
-	sleep(10000);
-	return "Hello start";
+	// sleep(10000);
+	// return "Hello start";
+	return content;
 }
 
 function upload() {
