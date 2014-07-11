762643ce8fee9fb754049a2faf206bba108b34e1
xieQin <xieqin1991@gmail.com>
2014-07-11 10:34:41 +0800

    add post

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,30 @@
-var exec = require("child_process").exec;
+// var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response,postData) {
 	console.log("Request handler 'start' was called.");
 	// var content = "empty";
+	var body = '<html>' +
+			   '<head>' +
+			   '<meta http-equiv="Content-Type" content="text/html;' +
+			   'charset=UTF-8" />' +
+			   '</head>' +
+			   '<body>' +
+			   '<form action="/upload" method="post">' +
+			   '<textarea name="text" rows="20" cols="60"></textarea>' +
+			   '<input type="submit" value="Submit text" />' +
+			   '</form>' +
+			   '</body>' +
+			   '</html>';
 
-	exec("ls-lah", function(error, stdout, stderr) {
+	// exec("ls", function(error, stdout, stderr) {
+	// exec("find /", {timeout: 10000, maxBuffer: 20000*1024}, function(error, stdout, stderr) {
 		// content = stdout;
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
+		response.writeHead(200, {"Content-Type": "text/html"});
+		// response.write(stdout);
+		response.write(body);
 		response.end();
-	});
+	// });
 	// function sleep(milliSeconds) {
 	// 	var starTime = new Date().getTime();
 	// 	while(new Date().getTime() < starTime + milliSeconds);
@@ -20,10 +35,10 @@ function start(response) {
 	// return content;
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent:" + querystring.parse(postData).text);
 	response.end();
 	// return "Hello upload";
 }
--- server.js
+++ server.js
@@ -9,13 +9,25 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		// console.log(url.parse(request.url));
 		console.log("Request for " + pathname + " received.");
 
+		request.setEncoding("utf8");
+
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + postDataChunk + "'.");
+		});
+
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
+
 		// response.writeHead(200, {"Content-Type": "text/plain"});
 		// var content = route(handle, pathname)
-		route(handle, pathname, request, response);
+		// route(handle, pathname, request, response);
 		// response.write(content);
 		// response.end();
 	}
